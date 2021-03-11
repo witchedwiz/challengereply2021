288d0dc3a81274cf42f39d850dacdbf820c6ef50
Davide <Melchionda>
2021-03-11 18:42:40 +0100

    with constructor

--- src/main/java/it/reply/challenge/edition2021/model/Antenna.java
+++ src/main/java/it/reply/challenge/edition2021/model/Antenna.java
@@ -1,8 +1,10 @@
 package it.reply.challenge.edition2021.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Antenna {
 
     Integer range;
--- src/main/java/it/reply/challenge/edition2021/model/Building.java
+++ src/main/java/it/reply/challenge/edition2021/model/Building.java
@@ -1,9 +1,11 @@
 package it.reply.challenge.edition2021.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Building {
 
     int x;
