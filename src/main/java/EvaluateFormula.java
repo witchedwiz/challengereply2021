bae963d176d65703f4f51403965a4868515745fe
v.delrosso <v.delrosso@reply.it>
2021-03-11 19:33:44 +0100

    init

---
+++ src/main/java/EvaluateFormula.java
@@ -0,0 +1,27 @@
+import it.reply.challenge.edition2021.model.Antenna;
+import it.reply.challenge.edition2021.model.Building;
+
+import java.util.List;
+
+public class EvaluateFormula {
+
+
+    public static void pairElaboration(List<Building> buildingsList, List<Antenna> antennaeList,
+            int maxX, int maxY) {
+
+        for (int i = 0; i < maxX; i++) {
+            for (int y = 0; y < maxY; y++) {
+                for (Building aBuilding : buildingsList) {
+                    for (Antenna aAntenna : antennaeList) {
+                        aAntenna.setX(i);
+                        aAntenna.setY(y);
+                        Integer distance = Math.abs(aAntenna.getX() - aBuilding.getX()) + Math.abs(aAntenna.getY() - aBuilding.getY());
+                        Integer score = (aBuilding.getConnSpeedWeight() * aAntenna.getConnSpeed()) - (aBuilding.getLatencyWeight() * distance);
+                        scoreList.add(score);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ src/main/java/it/reply/challenge/edition2021/model/InterestingConf.java
@@ -0,0 +1,8 @@
+package it.reply.challenge.edition2021.model;
+
+import java.util.Map;
+
+public class InterestingConf {
+    Map<String,Cell> configuration;
+
+}
